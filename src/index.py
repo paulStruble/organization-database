201e0c8b225376ae860a1527b52ae08b32e9d673
pjstruble1@gmail.com <pjstruble1@gmail.com>
2023-10-31 21:45:02 -0700

    index.py updates (defunct)

--- src/index.py
+++ src/index.py
@@ -69,31 +69,47 @@ class Organization:
 
 
 class OrgDriver:
-    # input and output files
-    csv_path = "data/org_csv_databases/general_data.csv"
-    json_path = "data/org_json_graphs/general_data.json"
+    @staticmethod
+    # converts a csv string input organization to a json graph output
+    def org_csv_to_json(csv_str: str) -> str:
+        # store data from CSV input as an Organization object
+        org = Organization()
 
-    # store data from CSV file as Organization object
-    org = Organization()
-
-    # read in CSV
-    with open(csv_path) as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            salary = int(row["salary"])
-            isManager = row["isManager"] == "TRUE"
-            employee = Employee(row["name"], row["department"], salary, row["office"], isManager, [row["skill1"], row["skill2"], row["skill3"]])
+        # parse csv string input to Organization object
+        rows = "\n".split(csv_str)
+        for row_index in range(1, len(rows) - 1):
+            row = ",".split(rows[row_index])
+            salary = int(row[2])
+            isManager = row[4] == "TRUE"
+            employee = Employee(row[0], row[1], salary, row[3], isManager, [row[5], row[6], row[7]])
             org.add_employee(employee)
 
-    # write to JSON and reformat
-    with open(json_path, 'w') as json_file:
-        json_file.write(org.to_json())
+        # convert Organization object to JSON string
+        return org.to_json()
 
-    with open(json_path, 'r') as json_file:
-        json_data = json.load(json_file)
-        print(f'json_data type: {type(json_data)}')
-
-    with open(json_path, 'w') as json_file:
-        json.dump(json_data, json_file, indent=4)
-
-    print(f'CSV file "{csv_path}" has been converted to JSON file "{json_path}"')
+    # @staticmethod
+    # def csv_to_json_filepath(csv_path: str, json_path: str):
+    #     # store data from CSV file as Organization object
+    #     org = Organization()
+    #
+    #     # read in CSV
+    #     with open(csv_path) as csv_file:
+    #         reader = csv.DictReader(csv_file)
+    #         for row in reader:
+    #             salary = int(row["salary"])
+    #             isManager = row["isManager"] == "TRUE"
+    #             employee = Employee(row["name"], row["department"], salary, row["office"], isManager, [row["skill1"], row["skill2"], row["skill3"]])
+    #             org.add_employee(employee)
+    #
+    #     # write to JSON and reformat
+    #     with open(json_path, 'w') as json_file:
+    #         json_file.write(org.to_json())
+    #
+    #     with open(json_path, 'r') as json_file:
+    #         json_data = json.load(json_file)
+    #         print(f'json_data type: {type(json_data)}')
+    #
+    #     with open(json_path, 'w') as json_file:
+    #         json.dump(json_data, json_file, indent=4)
+    #
+    #     print(f'CSV file "{csv_path}" has been converted to JSON file "{json_path}"')
